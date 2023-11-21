93a41ee04fd822f9259668c0b88b87922e772f35
Santiago <santimondragonm@gmail.com>
2023-11-20 23:07:06 -0500

    Contact form capabilities added}

--- src/app/[locale]/components/PageSection/Contact/Contact.tsx
+++ src/app/[locale]/components/PageSection/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, type FormEvent } from "react";
+import { useState, type FormEvent, useEffect } from "react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
@@ -10,21 +10,67 @@ import { Button } from "../../Button";
 
 import styles from "./Contact.module.scss";
 
+import emailjs from "@emailjs/browser";
+
 export const Contact = () => {
   const t = useTranslations("contact-us");
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [moreInfo, setMoreInfo] = useState("");
-  const handleSubmit = (e: FormEvent) => {
+  const [isSending, setSending] = useState(false);
+  const [isSent, setSent] = useState(false);
+  const [isError, setError] = useState(false);
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    alert(
-      `Hi ${name} ${lastName}.\nWe will soon contact you to ${email} about: ${moreInfo}`
-    );
+    setSending(true);
+
+    const templateParams = {
+      name: name,
+      email: email,
+      lastname: lastName,
+      subject: moreInfo,
+    };
+
+    try {
+      const { status } = await emailjs.send(
+        "default_service",
+        "template_efx47rn",
+        templateParams,
+        "SOs7nlEDZqSNcqyP_"
+      );
+
+      if (status === 200) {
+        setSending(false);
+        setSent(true);
+        alert(
+          `Hi ${name} ${lastName}.\nWe will soon contact you to ${email} about: ${moreInfo}`
+        );
+      } else {
+        setSending(false);
+        setError(true);
+        alert(
+          `Oops! Something went wrong with the contact form.\n Please try again later.`
+        );
+      }
+    } catch (error) {
+      setSending(false);
+      setError(true);
+      alert(
+        `Oops! Something went wrong with the contact form.\n Please try again later.`
+      );
+    }
+
     setName("");
     setLastName("");
     setEmail("");
     setMoreInfo("");
+
+    setTimeout(() => {
+      setSent(false);
+      setError(false);
+    }, 1000 * 5);
   };
 
   const formData = [
@@ -52,35 +98,62 @@ export const Contact = () => {
     "collaborate",
   ] as const;
 
+  const renderContent = () => {
+    if (isSending) {
+      return <div className={styles.contact__content__body}>Sendind...</div>;
+    } else if (isSent) {
+      return (
+        <div className={styles.contact__content__body}>
+          Thank you for contacting us! <br />
+          We will soon be contacting you.
+        </div>
+      );
+    } else if (isError) {
+      return (
+        <div className={styles.contact__content__body}>
+          Sorry! Something went wrong with the contact form. <br />
+          Plase try again later.
+        </div>
+      );
+    } else {
+      return (
+        <form
+          className={styles.contact__form}
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          {formData.map((data) => (
+            <TextInput
+              key={t(data.input)}
+              label={t(data.input)}
+              name={data.input}
+              value={data.value}
+              onChange={data.setFunction}
+              theme={eInputTheme.light}
+            />
+          ))}
+          <SelectInput
+            label={t("information")}
+            name="information"
+            value={moreInfo}
+            onChange={setMoreInfo}
+            options={selectOptions.map((option) => t(`options.${option}`))}
+            theme={eInputTheme.light}
+          />
+          <Button htmlType="submit" fullWidth>
+            {t("send-btn")}
+          </Button>
+        </form>
+      );
+    }
+  };
+
   return (
     <article className={styles.contact}>
       <div className={styles.contact__content}>
         <h2 className={styles.contact__content__title}>{t("title")}</h2>
         <p className={styles.contact__content__body}>{t("description")}</p>
       </div>
-      <form className={styles.contact__form} onSubmit={(e) => handleSubmit(e)}>
-        {formData.map((data) => (
-          <TextInput
-            key={t(data.input)}
-            label={t(data.input)}
-            name={data.input}
-            value={data.value}
-            onChange={data.setFunction}
-            theme={eInputTheme.light}
-          />
-        ))}
-        <SelectInput
-          label={t("information")}
-          name="information"
-          value={moreInfo}
-          onChange={setMoreInfo}
-          options={selectOptions.map((option) => t(`options.${option}`))}
-          theme={eInputTheme.light}
-        />
-        <Button htmlType="submit" fullWidth>
-          {t("send-btn")}
-        </Button>
-      </form>
+      {renderContent()}
       <Image
         className={styles["contact__paint-top"]}
         src="/paints/contact-top.png"
